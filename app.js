68cbe77f2039d6266b8c18034585a1336fc2d0ba
srinadhuSrikanya <93194424+srinadhuSrikanya@users.noreply.github.com>
2023-02-28 19:29:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const add =require("./utils.js")
+const sum=add(4,-2)
+
+
+console.log(sum)
+
+// const fs=require('fs')
+// // fs.appendFileSync('note.txt','my name is srikanya')
+// fs.writeFileSync('note.txt','i live in vizag')
---
+++ utils.js
@@ -0,0 +1,7 @@
+console.log('utils.js')
+const name='mike'
+const add=function(a,b){
+    return a+b
+}
+
+module.exports =add
